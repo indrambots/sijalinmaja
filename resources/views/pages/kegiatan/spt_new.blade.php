d59427bc7ac2f4828fc04628b0ebbe466eca1ac4
DESKTOP-TL3QERH\sngrindra <indra.prasetya.hening@gmail.com>
2023-01-10 17:18:03 +0700

    print spt

--- resources/views/pages/kegiatan/spt_new.blade.php
+++ resources/views/pages/kegiatan/spt_new.blade.php
@@ -19,7 +19,7 @@
     .page {
         width: 210mm;
         min-height: 297mm;
-        padding: 20mm;
+        padding: 15mm;
         margin: 10mm auto;
         border: 1px #D3D3D3 solid;
         border-radius: 5px;
@@ -58,6 +58,13 @@
  h1 { color: black; font-family:Arial, sans-serif; font-style: normal; font-weight: bold; text-decoration: none; font-size: 18pt; }
  h5 { color: black; font-family:"Times New Roman", sans-serif; font-style: normal; font-weight: bold; text-decoration: underline; font-size: 14pt; line-height:0pt; }
  a { color: #00AEEE; font-family:"Times New Roman", serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 11pt; }
+ .s2 { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: bold; text-decoration: underline; font-size: 14pt; }
+ .s3 { color: black; font-family:Arial, sans-serif; font-style: normal; font-weight: bold; text-decoration: underline; font-size: 11pt; }
+ .s4 { color: black; font-family:Arial, sans-serif; font-style: normal; font-weight: bold; text-decoration: none; font-size: 11pt; }
+ .s5 { color: black; font-family:Arial, sans-serif; font-style: normal; font-weight: bold; text-decoration: none; font-size: 12pt; }
+ .s6 { color: black; font-family:Arial, sans-serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 12pt; }
+ h3 { color: black; font-family:Arial, sans-serif; font-style: normal; font-weight: bold; text-decoration-line: underline; text-decoration-thickness: 2px;font-size: 12pt; }
+ table, tbody {vertical-align: top; overflow: visible; }
 </style>
 </head>
 <body>
@@ -65,7 +72,7 @@
     <div class="page">
     	<table style="overflow:visible;">
     		<tr>
-    			<td><img alt="image" height="112" src="{{asset('media/bg/logo_jatim.gif')}}" width="74"/></td>
+    			<td style="width:80pt"><img alt="image" height="112" src="{{asset('media/bg/logo_jatim.gif')}}" width="74"/></td>
     			<td style="text-align: center; line-height: 1px">
                         <h3 style="font-weight: bold; line-height:5pt;">PEMERINTAH PROVINSI JAWA TIMUR</h3>
                         <h1 style="line-height:5pt;">SATUAN POLISI PAMONG PRAJA</h1>
@@ -81,12 +88,262 @@
                     </td>
     		</tr>
     	</table>
-    	<table>
-    		<tr>
-    			<td><p>1.</p></td>
-    			<td><p>Peraturan Daerah Provinsi Jawa Timur Nomor 14 Tahun 2022 tentang Anggaran Pendapatan dan Belanja Daerah Provinsi Jawa Timur Tahun Anggaran 2023;</p></td>
-    		</tr>
-    	</table>
+
+        <p class="s3" style="ptext-indent: -2pt;line-height: 125%;text-align: center;">
+            SURAT PERINTAH TUGAS</p>
+            <p style="text-align: center;">
+                Nomor : 094/751/106.1/2022
+            </p> <br>
+        </p>
+    	<table cellspacing="0" style="border-collapse:collapse; margin-bottom: -55px;">
+            <tr style="height:43pt">
+                <td style="width:61pt">
+                    <p class="s4" style="padding-left: 2pt;text-indent: 0pt;line-height: 12pt;text-align: left;">
+                        D A S A R
+                    </p>
+                </td>
+                <td style="width:18pt">
+                    <p class="s5" style="padding-left: 1pt;text-indent: 0pt;line-height: 14pt;text-align: center;">
+                        :
+                    </p>
+                </td>
+                <td style="width:24pt">
+                    <p class="s6" style="padding-left: 4pt;padding-right: 5pt;text-indent: 0pt;line-height: 14pt;text-align: center;">
+                        1.
+                    </p>
+                </td>
+                <td colspan="3" style="width:421pt">
+                    <p class="s6" style="padding-left: 8pt;padding-right: 2pt;text-indent: 0pt;text-align: justify;">
+                        Peraturan Daerah Provinsi Jawa Timur Nomor 14 Tahun 2022 tentang Anggaran Pendapatan dan Belanja Daerah Provinsi Jawa Timur Tahun Anggaran 2023;
+                    </p>
+                </td>
+            </tr>
+            <tr style="height:43pt">
+                <td style="width:61pt">
+                    <p style="text-indent: 0pt;text-align: left;">
+                        <br/>
+                    </p>
+                </td>
+                <td style="width:18pt">
+                    <p style="text-indent: 0pt;text-align: left;">
+                        <br/>
+                    </p>
+                </td>
+                <td style="width:24pt">
+                    <p class="s6" style="padding-left: 4pt;padding-right: 5pt;text-indent: 0pt;text-align: center;">
+                        2.
+                    </p>
+                </td>
+                <td colspan="3" style="width:421pt">
+                    <p class="s6" style="padding-left: 8pt;padding-right: 2pt;text-indent: 0pt;line-height: 14pt;text-align: justify;">
+                        Peraturan Gubernur Jawa Timur Nomor  66 Tahun 2022 tentang Pedoman Kerja dan Pelaksanaan Tugas Pemerintah Provinsi Jawa Timur Tahun 2023;
+
+                    </p>
+                </td>
+            </tr>
+            <tr style="height:43pt">
+                <td style="width:61pt">
+                    <p style="text-indent: 0pt;text-align: left;">
+                        <br/>
+                    </p>
+                </td>
+                <td style="width:18pt">
+                    <p style="text-indent: 0pt;text-align: left;">
+                        <br/>
+                    </p>
+                </td>
+                <td style="width:24pt">
+                    <p class="s6" style="padding-left: 4pt;padding-right: 5pt;text-indent: 0pt;text-align: center;">
+                        3.
+                    </p>
+                </td>
+                <td colspan="3" style="width:421pt">
+                    <p class="s6" style="padding-left: 8pt;text-indent: 0pt;text-align: left;">
+                        Peraturan Gubernur Jawa Timur Nomor 89 Tahun 2022 tentang Penjabaran Anggaran Pendapatan dan Belanja Daerah Provinsi Jawa Timur Tahun Anggaran 2023
+                    </p>
+                </td>
+            </tr>
+            <tr style="height:88pt">
+                <td style="width:61pt">
+                    <p style="text-indent: 0pt;text-align: left;">
+                        <br/>
+                    </p>
+                </td>
+                <td style="width:18pt">
+                    <p style="text-indent: 0pt;text-align: left;">
+                        <br/>
+                    </p>
+                </td>
+                <td style="width:24pt">
+                    <p class="s6" style="padding-top: 2pt;padding-left: 6pt;text-indent: 0pt;text-align: left;">
+                        4.
+                    </p>
+                    <p style="text-indent: 0pt;text-align: left;">
+                        <br/>
+                    </p>
+                </td>
+                <td colspan="3" style="width:421pt">
+                    <p class="s6" style="padding-top: 2pt;padding-left: 8pt;padding-right: 2pt;text-indent: 0pt;text-align: justify;">
+                        Dokumen Pelaksanaan Anggaran Satuan Polisi Pamong Praja Provinsi Jawa Timur Tahun Anggaran 2023 Nomor DPA/A.1/1.05.0.00.0.00.01.0000/001/2023
+                    </p>
+                </td>
+            </tr>
+        </table>
+            <p class="s5" style="text-align: center;">
+                M E M E R I N T A H K A N
+            </p>
+            <br>
+    	<table cellspacing="0" style="border-collapse:collapse;">
+            <tr style="height:20pt">
+                <td style="width:61pt">
+                    <p class="s4" style="padding-left: 2pt;text-indent: 0pt;line-height: 12pt;text-align: left;">
+                        KEPADA
+                    </p>
+                </td>
+                <td style="width:18pt">
+                    <p class="s5" style="padding-left: 1pt;text-indent: 0pt;line-height: 14pt;text-align: center;">
+                        :
+                    </p>
+                </td>
+                <td colspan="3" >
+                    <p class="s6" style="padding-left: 4pt;padding-right: 5pt;text-indent: 0pt;line-height: 14pt;">
+                         Daftar nama-nama terlampir
+                    </p>
+                </td>
+            </tr>
+            <tr style="height:43pt">
+                <td style="width:61pt">
+                    <p class="s4" style="padding-left: 2pt;text-indent: 0pt;line-height: 12pt;text-align: left;">
+                        UNTUK
+                    </p>
+                </td>
+                <td style="width:18pt">
+                    <p class="s5" style="padding-left: 1pt;text-indent: 0pt;line-height: 14pt;text-align: center;">
+                        :
+                    </p>
+                </td>
+                <td style="width:24pt">
+                    <p class="s6" style="padding-left: 4pt;padding-right: 5pt;text-indent: 0pt;line-height: 14pt;text-align: center;">
+                        1.
+                    </p>
+                </td>
+                <td colspan="3" style="width:421pt">
+                    <p class="s6" style="padding-left: 8pt;padding-right: 2pt;text-indent: 0pt;text-align: justify;">
+                       Melaksanakan Tugas Pengamanan Pergantian Malam Tahun Baru 2023 pada Hari Sabtu Tanggal 31 Desember 2022 di Gedung Negara Grahadi Surabaya, Pukul 19.00 WIB;
+                    </p>
+                </td>
+            </tr>
+            <tr>
+                <td style="width:61pt">
+                    <p class="s4" style="padding-left: 2pt;text-indent: 0pt;line-height: 12pt;text-align: left;">
+                        <br>
+                    </p>
+                </td>
+                <td style="width:18pt">
+                    <p class="s5" style="padding-left: 1pt;text-indent: 0pt;line-height: 14pt;text-align: center;">
+                        <br>
+                    </p>
+                </td>
+                <td style="width:24pt">
+                    <p class="s6" style="padding-left: 4pt;padding-right: 5pt;text-indent: 0pt;line-height: 14pt;text-align: center;">
+                        2.
+                    </p>
+                </td>
+                <td colspan="3" style="width:421pt">
+                    <p class="s6" style="padding-left: 8pt;padding-right: 2pt;text-indent: 0pt;text-align: justify;">
+                       APP di Kantor Satuan Polisi Pamong Praja Provinsi Jawa Timur Jl. Jagir Wonokromo No.352 Surabaya, Pukul : 17.00 WIB
+                    </p>
+                </td>
+            </tr>
+            <tr >
+                <td style="width:61pt">
+                    <p class="s4" style="padding-left: 2pt;text-indent: 0pt;line-height: 12pt;text-align: left;">
+                        <br>
+                    </p>
+                </td>
+                <td style="width:18pt">
+                    <p class="s5" style="padding-left: 1pt;text-indent: 0pt;line-height: 14pt;text-align: center;">
+                        <br>
+                    </p>
+                </td>
+                <td style="width:24pt">
+                    <p class="s6" style="padding-left: 4pt;padding-right: 5pt;text-indent: 0pt;line-height: 14pt;text-align: center;">
+                        3.
+                    </p>
+                </td>
+                <td colspan="3" style="width:421pt">
+                    <p class="s6" style="padding-left: 8pt;padding-right: 2pt;text-indent: 0pt;text-align: justify;">
+                       Pakaian PDL II + Topi Pet + Rompi Hitam yang terbaru
+                    </p>
+                </td>
+            </tr>
+            <tr >
+                <td style="width:61pt">
+                    <p class="s4" style="padding-left: 2pt;text-indent: 0pt;line-height: 12pt;text-align: left;">
+                        <br>
+                    </p>
+                </td>
+                <td style="width:18pt">
+                    <p class="s5" style="padding-left: 1pt;text-indent: 0pt;line-height: 14pt;text-align: center;">
+                        <br>
+                    </p>
+                </td>
+                <td style="width:24pt">
+                    <p class="s6" style="padding-left: 4pt;padding-right: 5pt;text-indent: 0pt;line-height: 14pt;text-align: center;">
+                        4.
+                    </p>
+                </td>
+                <td colspan="3" style="width:421pt">
+                    <p class="s6" style="padding-left: 8pt;padding-right: 2pt;text-indent: 0pt;text-align: justify;">
+                       Melaporkan hasil Pelaksanaan Tugas tersebut kepada Kepala Satuan Polisi Pamong Praja Provinsi Jawa Timur.
+                    </p>
+                </td>
+            </tr>
+            <tr style="height:36pt">
+                <td style="width:61pt">
+                    <p style="text-indent: 0pt;text-align: left;">
+                        <br/>
+                    </p>
+                </td>
+                <td style="width:18pt">
+                    <p style="text-indent: 0pt;text-align: left;">
+                        <br/>
+                    </p>
+                </td>
+                <td colspan="4" style="width:445pt">
+                    <p class="s6" style="padding-top: 6pt;padding-left: 4pt;text-indent: 18pt;line-height: 14pt;text-align: left;">
+                        Demikian Surat Perintah Tugas ini untuk dilaksanakan dengan sebaik- baiknya dengan penuh rasa tanggung jawab.                         `
+                    </p>
+                </td>
+            </tr>
+        </table>
+
+        <p style="padding-top: 4pt;padding-left: 322pt;text-indent: 0pt;text-align: left;">
+            Ditetapkan di : Surabaya<br>
+            <b style="border-bottom:2px solid black">
+                Pada tanggal : 8 Juni 2022
+            </b>
+        </p> <br>
+        <p style="padding-top: 4pt;padding-left: 250pt;text-indent: 0pt;text-align: center;">
+            KEPALA SATUAN POLISI PAMONG PRAJA PROVINSI JAWA TIMUR
+        </p><br><br>
+        <h3 style="margin-bottom: -0.1px; padding-top: 3pt;padding-left: 225pt;text-indent: 0pt;text-align: center;">
+            M. HADI WAWAN GUNTORO, S.STP., M.Si., CIPA
+        </h3>
+        <p style="padding-left: 250pt;text-indent: 0pt;text-align: center;">
+            Pembina Utama Muda
+        </p>
+        <p style="padding-left: 250pt;text-indent: 0pt;text-align: center;">
+            NIP. 19770323 199511 1 001
+        </p>
+        <p style="text-indent: 0pt;text-align: left;">
+            <br/>
+        </p>
+        <p style="padding-left: 5pt;text-indent: 0pt;text-align: left;">
+            <span>
+                <img alt="image" height="55" src="{{asset()}}" width="768"/>
+            </span>
+        </p>
     </div>
 </body>
 </html>
