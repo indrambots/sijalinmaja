d74dd81811f6e84f80a95c79371aa24b99cdb093
Adjis Afandi <adjisweb@gmail.com>
2023-09-22 08:10:09 +0700

    bug fixing sarpras

--- resources/views/pages/anggaran-lembaga/kelembagaan/form-sarpras/index.blade.php
+++ resources/views/pages/anggaran-lembaga/kelembagaan/form-sarpras/index.blade.php
@@ -46,126 +46,136 @@
     <li class="breadcrumb-item px-3 text-muted">Data Sarpras</li>
 </ol>
 <div class="card">
-    <div class="card-body">
-        <div class="card-label"><h4>Data Sarpras</h4></div>
-        <hr>
-        <div class="row justify-content-between">
-            <div class="col-12">
-                <div class="d-flex justify-content-end">
-                    <a href="javascript:;" class="btn btn-primary m-b-xs">
-                        <i class="fas fa-save"></i> Simpan
-                    </a>&nbsp;
+    <form class="form" method="POST" action="{{url('anggaran/kelembagaan/sarpras/store')}}">
+        @csrf
+        @method('post')
+        <div class="card-body">
+            <div class="card-label"><h4>Data Sarpras</h4></div>
+            <hr>
+            <div class="row justify-content-between">
+                <div class="col-12">
+                    <div class="d-flex justify-content-end">
+                        <button type="submit" class="btn btn-primary m-b-xs">
+                            <i class="fas fa-save"></i> Simpan
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <hr>
-        <div class="row mt-2">
-            <div class="col-12">
-                <table class="table table-bordered table-striped table-sm">
-                    <thead>
-                        <tr class="text-center">
-                            <th rowspan="2"><strong>NO</strong></th>
-                            <th rowspan="2"><strong>JENIS SARANA DAN PRASARANA</strong></th>
-                            <th rowspan="2"><strong>JUMLAH SARANA DAN PRASANA</strong></th>
-                            <th colspan="2"><strong>KONDISI SARANA DAN PRASARANA</strong></th>
-                        </tr>
-                        <tr class="text-center">
-                            <th><strong>LAYAK</strong></th>
-                            <th><strong>TIDAK LAYAK</strong></th>
-                        </tr>
-                        <tr class="text-center">
-                            @for($i=1;$i<=5;$i++)
-                                <th style="background-color:#d0cecf">{{$i}}</th>
-                            @endfor
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php
-                            $grp1 = 'A';
-                        @endphp
-                        @foreach (Helpers::formSarpras() as $group1 => $sarpras)
-                            <tr>
-                                <td class="text-center bg-header"><strong>{{$grp1}}</strong></td>
-                                <td class="bg-header"><strong>{{$group1}}</strong></td>
-                                @for($i=1;$i<=3;$i++)
-                                    <td class="bg-header"></td>
+            <hr>
+            <div class="row mt-2">
+                <div class="col-12">
+                    <table class="table table-bordered table-striped table-sm">
+                        <thead>
+                            <tr class="text-center">
+                                <th rowspan="2"><strong>NO</strong></th>
+                                <th rowspan="2"><strong>JENIS SARANA DAN PRASARANA</strong></th>
+                                <th rowspan="2"><strong>JUMLAH SARANA DAN PRASANA</strong></th>
+                                <th colspan="2"><strong>KONDISI SARANA DAN PRASARANA</strong></th>
+                            </tr>
+                            <tr class="text-center">
+                                <th><strong>LAYAK</strong></th>
+                                <th><strong>TIDAK LAYAK</strong></th>
+                            </tr>
+                            <tr class="text-center">
+                                @for($i=1;$i<=5;$i++)
+                                    <th style="background-color:#d0cecf">{{$i}}</th>
                                 @endfor
                             </tr>
+                        </thead>
+                        <tbody>
                             @php
-                                $grp2 = 1;
+                                $grp1 = 'A';
                             @endphp
-                            @foreach($sarpras as $group2 => $sarp)
-                                @if($group2)
-                                    <tr>
-                                        <td class="text-center bg-header"><strong>{{Helpers::numberToRoman($grp2)}}</strong></td>
-                                        <td class="bg-header"><strong>{{$group2}}</strong></td>
-                                        @for($i=1;$i<=3;$i++)
-                                            <td class="bg-header"></td>
-                                        @endfor
-                                    </tr>
-                                    @php $grp2++; @endphp
-                                    @php $no2 = 1; @endphp
-                                    @foreach($sarp as $grp22 => $group22)
-                                        @foreach($group22 as $ids => $ss)
-                                            <tr>
-                                                <td class="text-center {!! $ss == 'Peralatan Komunikasi' ? 'bg-header' : '' !!}">{{$grp22 ? '' : $no2.'.'}}</td>
-                                                <td {!! $ss == 'Peralatan Komunikasi' ? 'class="bg-header"' : '' !!}>{{$ss}}</td>
-                                                @if($ss != 'Peralatan Komunikasi')
-                                                    <td><input type="number" name="jumlah[{{$ids}}]" min="0" class="input-custom"></td>
-                                                    <td class="text-center"><input type="radio" name="layak[{{$ids}}]" id="layak{{$ids}}"></td>
-                                                    <td class="text-center"><input type="radio" name="layak[{{$ids}}]" id="tidak_layak{{$ids}}"></td>
-                                                @else
+                            @foreach (Helpers::formSarpras() as $group1 => $sarpras)
+                                <tr>
+                                    <td class="text-center bg-header"><strong>{{$grp1}}</strong></td>
+                                    <td class="bg-header"><strong>{{$group1}}</strong></td>
+                                    @for($i=1;$i<=3;$i++)
+                                        <td class="bg-header"></td>
+                                    @endfor
+                                </tr>
+                                @php
+                                    $grp2 = 1;
+                                @endphp
+                                @foreach($sarpras as $group2 => $sarp)
+                                    @if($group2)
+                                        <tr>
+                                            <td class="text-center bg-header"><strong>{{Helpers::numberToRoman($grp2)}}</strong></td>
+                                            <td class="bg-header"><strong>{{$group2}}</strong></td>
+                                            @for($i=1;$i<=3;$i++)
+                                                <td class="bg-header"></td>
+                                            @endfor
+                                        </tr>
+                                        @php $grp2++; @endphp
+                                        @php $no2 = 1; @endphp
+                                        @foreach($sarp as $grp22 => $group22)
+                                            @foreach($group22 as $ids => $ss)
+                                                <tr>
+                                                    <td class="text-center {!! $ss == 'Peralatan Komunikasi' ? 'bg-header' : '' !!}">{{$grp22 ? '' : $no2.'.'}}</td>
+                                                    <td {!! $ss == 'Peralatan Komunikasi' ? 'class="bg-header"' : '' !!}>{{$ss}}</td>
+                                                    @if($ss != 'Peralatan Komunikasi')
+                                                        <td><input type="number" name="jumlah[{{$ids}}]" value="{{@$data[$ids]['jumlah']}}" min="0" class="input-custom"></td>
+                                                        <td class="text-center"><input type="radio" name="layak[{{$ids}}]" value="layak" {{@$data[$ids]['status_layak'] == 'layak' ? 'checked' : ''}} id="layak{{$ids}}" required></td>
+                                                        <td class="text-center">
+                                                            <input type="hidden" name="nama[{{$ids}}]" value="{{$ss}}">
+                                                            <input type="radio" name="layak[{{$ids}}]" value="tidak layak" {{@$data[$ids]['status_layak'] == 'tidak layak' ? 'checked' : ''}}  id="tidak_layak{{$ids}}" required>
+                                                        </td>
+                                                    @else
+                                                        @for($i=1;$i<=3;$i++)
+                                                            <td class="bg-header"></td>
+                                                        @endfor
+                                                    @endif
+                                                </tr>
+                                                @php $no2++; @endphp
+                                            @endforeach
+                                        @endforeach
+                                    @else
+                                        @foreach($sarp as $group3 => $s)
+                                            @if($group3)
+                                                <tr>
+                                                    <td></td>
+                                                    <td colspan="4">{{$group3}}</td>
                                                     @for($i=1;$i<=3;$i++)
                                                         <td class="bg-header"></td>
                                                     @endfor
-                                                @endif
-                                            </tr>
-                                            @php $no2++; @endphp
+                                                </tr>
+                                            @else
+                                                @php $no = 1; @endphp
+                                                    @foreach($s as $key => $d)
+                                                        <tr>
+                                                            <td class="text-center">{{$no}}.</td>
+                                                            <td>{{$d}}</td>
+                                                            <td><input type="number" name="jumlah[{{$key}}]" value="{{@$data[$key]['jumlah']}}" min="0" class="input-custom"></td>
+                                                            <td class="text-center"><input type="radio" name="layak[{{$key}}]" value="layak" {{@$data[$key]['status_layak'] == 'layak' ? 'checked' : ''}} id="layak{{$key}}" required></td>
+                                                            <td class="text-center">
+                                                                <input type="hidden" name="nama[{{$key}}]" value="{{$d}}">
+                                                                <input type="radio" name="layak[{{$key}}]" value="tidak layak" {{@$data[$key]['status_layak'] == 'tidak layak' ? 'checked' : ''}} id="tidak_layak{{$key}}" required>
+                                                            </td>
+                                                        </tr>
+                                                        @php $no++ @endphp
+                                                    @endforeach
+                                                @php $grp1++; @endphp
+                                            @endif
                                         @endforeach
-                                    @endforeach
-                                @else
-                                    @foreach($sarp as $group3 => $s)
-                                        @if($group3)
-                                            <tr>
-                                                <td></td>
-                                                <td colspan="4">{{$group3}}</td>
-                                                @for($i=1;$i<=3;$i++)
-                                                    <td class="bg-header"></td>
-                                                @endfor
-                                            </tr>
-                                        @else
-                                            @php $no = 1; @endphp
-                                                @foreach($s as $key => $data)
-                                                    <tr>
-                                                        <td class="text-center">{{$no}}.</td>
-                                                        <td>{{$data}}</td>
-                                                        <td><input type="number" name="jumlah[{{$key}}]" min="0" class="input-custom"></td>
-                                                        <td class="text-center"><input type="radio" name="layak[{{$key}}]" id="layak{{$key}}"></td>
-                                                        <td class="text-center"><input type="radio" name="layak[{{$key}}]" id="tidak_layak{{$key}}"></td>
-                                                    </tr>
-                                                    @php $no++ @endphp
-                                                @endforeach
-                                            @php $grp1++; @endphp
-                                        @endif
-                                    @endforeach
-                                @endif
+                                    @endif
+                                @endforeach
                             @endforeach
-                        @endforeach
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-        <hr>
-        <div class="row justify-content-between">
-            <div class="col-12">
-                <div class="d-flex justify-content-end">
-                    <a href="javascript:;" class="btn btn-primary m-b-xs">
-                        <i class="fas fa-save"></i> Simpan
-                    </a>&nbsp;
+            <hr>
+            <div class="row justify-content-between">
+                <div class="col-12">
+                    <div class="d-flex justify-content-end">
+                        <button type="submit" class="btn btn-primary m-b-xs">
+                            <i class="fas fa-save"></i> Simpan
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </form>
 </div>
 @endsection
 @section('script')
