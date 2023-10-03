464635dc2450d52a5e18f77c4f0f71812014b096
DESKTOP-TL3QERH\sngrindra <indra.prasetya.hening@gmail.com>
2023-10-03 16:28:57 +0700

    prevent responive landing page

--- resources/views/layouts/app_landing.blade.php
+++ resources/views/layouts/app_landing.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <!-- Place favicon.ico in the root directory -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/logo/sigap.png') }}">
     <meta name="description" content="Sistem Integrasi Jaga Lindungi Masyarakat Jawa Timur" />
